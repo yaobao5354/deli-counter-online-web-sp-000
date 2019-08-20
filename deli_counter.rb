1fe81c667c971021e61681ac5f863c8401f3a416
yaoyu bao <yaobao5354@gmail.com>
2019-08-20 04:03:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else 
-    pop = array.reverse.pop
+    pop = array.pop
     puts "Currently serving #{pop}."
   end 
 end 
