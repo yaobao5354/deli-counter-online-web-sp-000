841ce451da8f64fc66a4ef3af6ae887a5017bad9
yaoyu bao <yaobao5354@gmail.com>
2019-08-20 03:43:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+require 'pry'
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    collected_array = array.collect.with_index{|person, index| "#{index+1}. #{person}"}
+    puts "The line is currently: #{collected_array.join(" ")}"
+  end
+end 
+
+def take_a_number(array, name)
+  if array.length == 0 
+    array << name 
+    puts "Welcome, #{name}. You are number 1 in line."
+  else 
+    array << name
+    puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  end
+end 
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    array = array.reverse
+    puts "Currently serving #{array.pop}."
+  end 
+  return array.reverse
+end 
